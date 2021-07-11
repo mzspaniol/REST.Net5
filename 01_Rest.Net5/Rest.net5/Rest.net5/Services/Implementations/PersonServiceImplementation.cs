a752dc04d87d5c66c57ee96728ba1cdad7be58e6
Matheus <mzspaniol@gmail.com>
2021-07-11 13:31:46 -0300

    MySql Integration with Update, Insert, Delete and Select with API

--- 01_Rest.Net5/Rest.net5/Rest.net5/Services/Implementations/PersonServiceImplementation.cs
+++ 01_Rest.Net5/Rest.net5/Rest.net5/Services/Implementations/PersonServiceImplementation.cs
@@ -1,72 +1,91 @@
 ﻿using Rest.net5.Controllers.Model;
-using System;
 using System.Collections.Generic;
+using Rest.net5.Model.Context;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+using System;
 
 namespace Rest.net5.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
 
-    
-        private volatile int count;
+
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
             return person;
         }
 
         public void Delete(long id)
         {
-            
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> Persons = new List<Person> ();
-            for (int i = 0;  i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                Persons.Add(person);
-            }
-            return Persons;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "NamePerson" + i,
-                LastName = "LastNamePerson" + i,
-                Address = "Adress" + i,
-                Gender = "Masculino" + i
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-
-            return Interlocked.Increment(ref count);
+            return _context.Persons.ToList();
         }
 
         public Person FindByID(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Matheus",
-                LastName = "Spaniol",
-                Address = "pernambuco",
-                Gender = "Masculino"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
             return person;
+
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any((p => p.Id.Equals(id)));
         }
     }
 }
